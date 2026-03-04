bbc40dcdd92512a9c0a80ee1401178309e98e66b
anna <annamariagibson1708@gmail.com>
2026-03-04 13:21:55 +0530

    Q2

---
+++ level1/Q2.java
@@ -0,0 +1,14 @@
+public class Q2 {
+    public static void main(String[] args) {
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+        
+        // Calculating the average
+        double average = (maths + physics + chemistry) / 3.0;
+
+        System.out.println("Sam's average mark in PCM is " + average);
+    }
+}
+    
+
