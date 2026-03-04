d3a6e9ab8b05781474153e251abcb6a4e37e9c71
anna <annamariagibson1708@gmail.com>
2026-03-04 13:30:46 +0530

    Q1

---
+++ level2/Q1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Taking user inputs
+        System.out.print("Enter first number: ");
+        double number1 = input.nextDouble();
+        
+        System.out.print("Enter second number: ");
+        double number2 = input.nextDouble();
+
+        // Performing Arithmetic Operations
+        double add = number1 + number2;
+        double sub = number1 - number2;
+        double mul = number1 * number2;
+        double div = number1 / number2;
+
+        // Final Output
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers " 
+                            + number1 + " and " + number2 + " is " 
+                            + add + ", " + sub + ", " + mul + ", and " + div);
+        
+        input.close();
+    }
+}
