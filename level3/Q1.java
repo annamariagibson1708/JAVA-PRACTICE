c643dd19ac94103bee482d13a300eca1a50d8e1e
anna <annamariagibson1708@gmail.com>
2026-03-26 14:48:20 +0530

    Q1

---
+++ level3/Q1.java
@@ -0,0 +1,22 @@
+/*Create a program to take a number as input find the frequency of each digit in the number using an array and
+ display the frequency of each digit
+Hint => 
+Take the input for a number
+Find the count of digits in the number
+Find the digits in the number and save them in an array
+Find the frequency of each digit in the number. For this define a frequency array of size 10, Loop through the digits array, 
+and increase the frequency of each digit
+Display the frequency of each digit in the number
+ */
+
+import java.util.Scanner;
+
+public class Q1{
+
+    public static void main(String[] args)
+        {
+            Scanner input =new Scanner(System.in);
+        }
+    
+
+}
