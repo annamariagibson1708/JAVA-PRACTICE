4d1af999e70ad540021fea198ebba3f48fb08ce5
anna <annamariagibson1708@gmail.com>
2026-04-10 01:50:35 +0530

    Q2

---
+++ level1/Q2.java
@@ -0,0 +1,21 @@
+
+    import java.util.Scanner;
+public class Q2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of students: ");
+        int numberOfStudents = scanner.nextInt();
+
+        int handshakes = calculateHandshakes(numberOfStudents);
+
+        System.out.println("The number of possible handshakes is: " + handshakes);
+    }
+
+    // Method using the combination formula: (n * (n - 1)) / 2
+    public static int calculateHandshakes(int n) {
+        return (n * (n - 1)) / 2;
+    }
+}
+
